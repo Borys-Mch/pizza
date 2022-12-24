2246442e5589d9ca5ad75db382e541871c8dcecc
Borys Mch <borys.mch@gmail.com>
2022-12-24 06:19:53 +0200

    Added add product module

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,7 +1,7 @@
 import dbConnect from "../../../lib/mongo";
 import Order from "../../../models/Order";
 
-export default async function handler(req, res) {
+const handler = async (req, res) => {
   const {
     method,
     query: { id },
@@ -29,4 +29,6 @@ export default async function handler(req, res) {
   }
   if (method === "DELETE") {
   }
-}
+};
+
+export default handler;
